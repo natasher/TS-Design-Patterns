ae3af1e93d8a19391ec1e1a93ae9791a91fdc7f9
Artur Woźniak <artur.wozniak69@gmail.com>
2020-10-26 12:22:56 +0100

    Short overview of Creational Patterns
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

---
+++ Wzorce kreacyjne/readme.ts
@@ -0,0 +1,30 @@
+/**
+ * Wzorce kreacyjne są źródłem różnych mechanizmów tworzenia obiektów,
+ * zwiększających elastyczność i ułatwiających ponowne użycie kody.
+ *
+ * => Factory Method (Metoda wytwórcza)
+ *
+ *    Udostępnia interfejs do tworzenia obiektów w klasie bazowej,
+ *    ale pozwala podklasom zmieniać typ tworzonych obiektów.
+ *
+ * => Abstract Factory (Fabryka abstrakcyjna)
+ *
+ *    Umożliwia tworzenie rodzin spokrewnionych ze sobą
+ *    obiektów bez określania ich konkretnych klas.
+ *
+ * => Builder (Budowniczy)
+ *
+ *    Daje możliwość konstruowania złożonych obiektów krok po kroku.
+ *    Wzorzec ten pozwala produkować różne typy oraz reprezentacje
+ *    obiektu używając tego samego kodu konstrukcyjnego.
+ *
+ * => Prototype (Prototyp)
+ *
+ *    Umożliwia kopiowanie istniejących obiektów bez tworzenia
+ *    zależności pomiędzy twoim kodem, a ich klasami.
+ *
+ * => Singleton
+ *
+ *    Pozwala zachować pewność, że istnieje wyłącznie jedna instancja
+ *    danej klasy oraz istnieje dostęp do niej w przestrzeni globalnej.
+ */
